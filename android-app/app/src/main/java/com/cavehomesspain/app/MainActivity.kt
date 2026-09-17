273d6b389a20f16c9c6decccd487e39685740243
cavehomes <paulpheasant@hotmail.com>
2026-09-17 19:21:10 +0200

    Add native Android app activity

---
+++ android-app/app/src/main/java/com/cavehomesspain/app/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.cavehomesspain.app
+
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        webView = WebView(this)
+        setContentView(webView)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+        webView.loadUrl("https://cavehomesspain.com/app/")
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        if (webView.canGoBack()) webView.goBack() else super.onBackPressed()
+    }
+}
